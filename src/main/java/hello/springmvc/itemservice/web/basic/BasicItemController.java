ba64bc898350f2bcf6cb335ddd72c6de82d88b7f
gialeee <gia.lee@gmail.com>
2023-12-15 17:03:56 +0900

    상품 컨트롤러 생성

---
+++ src/main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,42 @@
+package hello.springmvc.itemservice.web.basic;
+
+import hello.springmvc.itemservice.domain.item.Item;
+import hello.springmvc.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // final인 필드에 대하여 생성자 자동 생성
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {  // @RequiredArgsConstructor 사용하여 생략 가능
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items");
+        return "basic/item";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct  // 해당 빈의 의존관계가 모두 주입되고 나면 초기화 용도로 호출
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
