7b0928ae84a9096ead6bf9d20518878b7c0119b2
gialeee <gia.lee@gmail.com>
2023-12-15 15:44:20 +0900

    initial commit

---
+++ src/main/java/hello/springmvc/itemservice/SpringmvcProjectApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcProjectApplication.class, args);
+    }
+
+}
