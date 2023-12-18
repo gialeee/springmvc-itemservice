b1438d6da846cbe30c7f7d05fa8ea51f7f8c1dac
gialeee <gia.lee@gmail.com>
2023-12-18 10:08:53 +0900

    상품 등록 처리 V2 추가 (@ModelAttribute)

--- src/main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
@@ -4,12 +4,9 @@ import hello.springmvc.itemservice.domain.item.Item;
 import hello.springmvc.itemservice.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,9 +25,50 @@ public class BasicItemController {
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
-        model.addAttribute("items");
+        model.addAttribute("items", items);
         return "basic/items";
     }
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add") // V2를 쓰기 위한 주석 처리
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     *  @ModelAttribute("item") Item item
+     *  model.addAttribute("item", item); 자동 추가, 생략 가능
+     * @ModelAttribute이 자동으로 Item 객체 생성 후 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로 입력해줌
+     */
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
 
     /**
      * 테스트용 데이터 추가
@@ -41,10 +79,4 @@ public class BasicItemController {
         itemRepository.save(new Item("itemB", 20000, 20));
     }
 
-    @GetMapping("/{itemId}")
-    public String item(@PathVariable Long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-        return "basic/item";
-    }
 }
